d5cb242e3442f4fdea1d29770eba5d785aeb0601
writa <kz.writa@gmail.com>
2013-04-27 23:58:44 +0900

    Add function max

---
+++ max.go
@@ -0,0 +1,11 @@
+package gotest
+
+func Max(nums []int) int {
+	tmp := 0
+	for _, i := range nums {
+		if tmp < i {
+			tmp = i
+		}
+	}
+	return tmp
+}
---
+++ max_test.go
@@ -0,0 +1,11 @@
+package gotest
+
+import "testing"
+
+func Test_Max(t *testing.T) {
+	nums := []int{0, 2, 3, 5, 4, 6}
+	max := Max(nums)
+	if max != 6 {
+		t.Error("error")
+	}
+}
