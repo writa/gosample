74dd0ec1c9d6c081721f4047490ab803a4cd44d8
writa <kz.writa@gmail.com>
2013-05-01 00:46:54 +0900

    Add sample pattern of flag package

--- flag.go
+++ flag.go
@@ -5,9 +5,13 @@ import (
 	"fmt"
 )
 
-var code *int = flag.Int("testcode", 716, "give me your codes")
+var intCode *int = flag.Int("int", 0, "int code")
+var strCode *string = flag.String("str", "default", "string code")
+var boolCode *bool = flag.Bool("bool", false, "bool code")
 
 func Flag() {
 	flag.Parse()
-	fmt.Println("testcode has value", *code)
+	fmt.Println("intCode has value", *intCode)
+	fmt.Println("strCode has value", *strCode)
+	fmt.Println("boolCode has value", *boolCode)
 }
